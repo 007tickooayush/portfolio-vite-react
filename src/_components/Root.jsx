1a908fc7eb0f2a8951ed9b3a918236f17e489df4
007tickooayush <ayush.tickoo292000@gmail.com>
2024-04-28 11:24:37 +0530

    routes added

--- src/_components/Root.jsx
+++ src/_components/Root.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link, Outlet } from 'react-router-dom';
 
 const Root = () => {
     return (
-        <div>Root</div>
+        <div>
+            My portfolio page
+            <nav>
+                <ul>
+                    <Link to="/home">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/contact">Contact</Link>
+                    {/* <li><a href="/home">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/projects">Projects</a></li>
+                    <li><a href="/contact">Contact</a></li> */}
+                </ul>
+            </nav>
+            <Outlet />
+        </div>
     )
 }
 
--- src/_utils/router.jsx
+++ src/_utils/router.jsx
@@ -1,12 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../_components/Root";
+import Home from "../_components/Home";
+import About from "../_components/About";
+import Projects from "../_components/Projects";
+import Contact from "../_components/Contact";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <Root />,
         children: [
-            
+            {
+                path: '/home',
+                element: <Home />
+            },
+            {
+                path: '/about',
+                element: <About />
+            },
+            {
+                path: '/projects',
+                element: <Projects />
+            },
+            {
+                path: '/contact',
+                element: <Contact />
+            }
         ]
     }
 ]);
