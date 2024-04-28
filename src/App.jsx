7b2d81f04471724b3c0ff0f1e7a62c57f465272c
007tickooayush <ayush.tickoo292000@gmail.com>
2024-04-28 11:14:19 +0530

    inital commit for project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+// import reactLogo from './_assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            My portfolio website
+        </>
+    )
+}
+
+export default App
