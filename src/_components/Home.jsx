97561af7f3513e1b32021c871c070fcc23fd75a2
007tickooayush <ayush.tickoo292000@gmail.com>
2024-04-29 22:05:08 +0530

    created home page details json file and basic homepage layout

--- src/_components/Home.jsx
+++ src/_components/Home.jsx
@@ -1,11 +1,28 @@
-import { Container, Text } from "@chakra-ui/react";
-
+import { Box, Container, SimpleGrid, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import homeDetails from '../_assets/home.json';
 
 const Home = () => {
-    
+
+    const [details, setDetails] = useState({});
+    useEffect(() => {
+        setDetails(homeDetails);
+    }, []);
 
     return (
         <Container maxW="container.xl" margin={4}>
+            <Text fontSize='5xl'>{details.name}</Text>
+            <SimpleGrid columns={2} spacing={20} mt={8} mb={8}>
+                <Box>
+                    <Text fontSize='md'>{details.description}</Text>
+                    <Text fontSize='md'>some description</Text>
+                </Box>
+                <Box alignItems={'end'}>
+                    <Text fontSize='2xl'>(skills)</Text>
+                    <Text fontSize='xl'>Ayush Tickoo (experience)</Text>
+                </Box>
+            </SimpleGrid>
+
             <Text fontSize='3xl'>Ayush Tickoo (desc)</Text>
             <Text fontSize='3xl'>Ayush Tickoo (experience detailed)</Text>
             <Text fontSize='3xl'>Ayush Tickoo (skills detailed)</Text>
