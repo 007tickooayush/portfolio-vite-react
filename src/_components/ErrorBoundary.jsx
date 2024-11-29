190dc2d17c9f8204c9c6ebbf720274fdcb9c92d4
007tickooayush <ayush.tickoo292000@gmail.com>
2024-11-29 22:42:50 +0530

    class based error boundary

--- src/_components/ErrorBoundary.jsx
+++ src/_components/ErrorBoundary.jsx
@@ -1,16 +1,29 @@
 import { Container, Flex, Text } from '@chakra-ui/react';
-import React from 'react'
+import React, { Component } from 'react'
 
-const ErrorBoundary = () => {
-    return (
-        <Container>
-            <Flex direction="column" justifyContent="center" alignItems="center" height="90vh">
-                <Text fontSize={'4xl'}>404</Text>
-                <Text fontSize={'3xl'}>Page Not Found</Text>
-                <Text>The requested page could not be found.</Text>
-            </Flex>
-        </Container>
-    );
+export default class ErrorBoundary extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error) {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error, errorInfo) {
+        console.error(error, errorInfo);
+    }
+
+    render() {
+        return (
+            <Container>
+                <Flex direction="column" justifyContent="center" alignItems="center" height="90vh">
+                    <Text fontSize={'4xl'}>404</Text>
+                    <Text fontSize={'3xl'}>Page Not Found</Text>
+                    <Text>The requested page could not be found.</Text>
+                </Flex>
+            </Container>
+        )
+    }
 }
-
-export default ErrorBoundary;
